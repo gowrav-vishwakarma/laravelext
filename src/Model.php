a648ea6db15b06c51143f6c7bce9e2f81b641b4f
Gowrav Viswhakarma <gowravvishwakarma@gmail.com>
2019-03-13 17:59:10 +0530

    Active/InActive Scopes and Validation class variable added

--- src/Model.php
+++ src/Model.php
@@ -7,8 +7,68 @@ use Carbon\Carbon;
 
 class Model extends EModel {
 
+	/**
+	 * To check if any model is in active_status is a very frequent requirement,
+	 * Values of each field can be scalar or array to check, all fields with ANDed operator and all values for same field with ORed
+	 * you can even set it to false or null to pass activity checks for models
+	 * 
+	 * Ex: $active_check = ['is_active'=>[1,'Yes'],'status'=>'Delivered']
+	 * 
+	 * @var array
+	 */
+	
+	public $active_check = ['is_active'=>1];
 
-// ======= Scope Methods based on CREATED_AT values =========== //
+	/**
+	 * Same as active_check but checks for Inactive in scope
+	 * @var array
+	 */
+	public $inactive_check = ['is_active'=>0];
+
+
+	public $validations	= null;
+
+
+	public function save(array $options = []){
+		if($this->validations && is_array($this->validations)) {
+			$validator = \Validator::make($this->toArray(), $this->validations);
+
+	        if ($validator->fails()) {
+	        	dd($validator->errors());
+	            // return redirect('post/create')
+	            //             ->withErrors($validator)
+	            //             ->withInput();
+	        }
+		}
+		parent::save($options);
+	}
+
+	// ======= Active InActive based scopes ===========//
+	function scopeActive($query){
+		if($this->active_check !== null || $this->active_check !== false){
+			foreach ($this->active_check as $field => $value) {
+				if(is_array($value)){
+					$query->whereIn($field,$value);
+				}else{
+					$query->where($field,$value);
+				}
+			}
+		}
+	}
+
+	function scopeInActive($query){
+		if($this->inactive_check !== null || $this->inactive_check !== false){
+			foreach ($this->inactive_check as $field => $value) {
+				if(is_array($value)){
+					$query->whereIn($field,$value);
+				}else{
+					$query->where($field,$value);
+				}
+			}
+		}
+	}
+
+	// ======= Scope Methods based on CREATED_AT values =========== //
 	/**
 	 * Use to apply scope based on date range INCLUDING TIME ALSO, like
 	 * $model->createdBetween('now','yesterday') or
@@ -66,7 +126,7 @@ class Model extends EModel {
 		$on_date = Carbon::parse($on_date);
 		$to_date = Carbon::parse($on_date)->addDay(1);
 		
-		$query->where($created_field,'>',$on_date->toDateString());
+		$query->where($created_field,'>=',$on_date->toDateString());
 		$query->where($created_field,'<',$to_date->toDateString());
 	}
 }
